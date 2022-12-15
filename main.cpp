3b6cba441d4d00530c62c5309a79a4d3e7b61258
Sagimune <50073535+Sagimune@users.noreply.github.com>
2022-12-15 16:50:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include "mainwindow.h"
-#include "segmenttree.h"
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    double X1[]={0,0,1};
-    double X2[]={0,2,3};
-    double Y1[]={0,0,1};
-    double Y2[]={0,2,3};
-    SegmentTree T;
-    std::pair<double,double> PP = T.Calc(2,X1,Y1,X2,Y2);
-    qDebug()<<PP.first<<" "<<PP.second;
     w.show();
     return a.exec();
 }
