28c670794f09fda8a3548fc3fead8497eb60fcd4
tdbqz <110575274+tdbqz@users.noreply.github.com>
2022-12-17 09:56:55 +0800

    界面设计

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include<qpushbutton.h>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->resize(1600,900);
+    ui->martixnum->setPlaceholderText(" 矩阵个数 ");
+    ui->x1->setPlaceholderText("x1");
+    ui->x2->setPlaceholderText("x2");
+    ui->y1->setPlaceholderText("y1");
+    ui->y2->setPlaceholderText("y2");
 }
 
 MainWindow::~MainWindow()
