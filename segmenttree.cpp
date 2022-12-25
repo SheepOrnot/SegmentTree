2d055fe9788f305d4829f307eb3be69bcb6970e4
Sagimune <50073535+Sagimune@users.noreply.github.com>
2022-12-25 09:23:48 +0800

    Update segmenttree.cpp

--- segmenttree.cpp
+++ segmenttree.cpp
@@ -6,6 +6,7 @@ SegmentTree::SegmentTree()
 }
 
 void SegmentTree::push_up(int x)
+// 更新tag
 {
     if(tree[x].cov)
     {
@@ -24,6 +25,7 @@ void SegmentTree::push_up(int x)
 }
 
 void SegmentTree::build(int x,int l,int r)
+// 建树加初始化
 {
     tree[x].l = l, tree[x].r = r;
     tree[x].cnt = tree[x].len = tree[x].cov = 0; tree[x].lc = tree[x].rc = 0;
@@ -35,6 +37,7 @@ void SegmentTree::build(int x,int l,int r)
 }
 
 void SegmentTree::update(int x,double L,double R,int flag)
+// 遍历时调用的更新函数
 {
     if(X[tree[x].r+1]<=L||X[tree[x].l]>=R) return;
     if(X[tree[x].l]>=L&&X[tree[x].r+1]<=R)
@@ -62,15 +65,20 @@ std::pair<double,double> SegmentTree::Calc(int InputN,double X1[],double Y1[],do
     std::sort(X+1,X+n+1);
     std::sort(line+1,line+n+1);
     m = std::unique(X+1,X+n+1)-X-1;
+    // 离散化横坐标，便于建树和计算
     build(1,1,m-1);
+    // 注意是1到m-1，每个节点表示范围为l~r+1，这样l=r的叶子节点也能表示区间，并且解决了上下限的问题
     double Area = 0,Perimeter = 0,pre = 0;
     for(int i = 1; i < n; ++i)
     {
         update(1,line[i].l,line[i].r,line[i].flag);
         Area += (tree[1].len*(line[i+1].h-line[i].h));
+        // 面积为扫描线投影*统计区间宽
         Perimeter += std::max(tree[1].len,pre)-std::min(tree[1].len,pre);
+        // 横边（相对于扫描线）长度求和，新增量为投影新增或新减量
         pre = tree[1].len;
         Perimeter += 2*(tree[1].cnt*(line[i+1].h-line[i].h));
+        // 竖边（相对于扫描线）长度求和，新增量为统计区间宽*投影连续段数*2
     }
     Perimeter += (line[n].r-line[n].l);
     return std::pair<double,double>(Area,Perimeter);
